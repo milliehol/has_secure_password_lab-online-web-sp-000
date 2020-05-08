38b939ba718c802643e663771a269df7cfec9763
Carla Holcomb <carla.programmer@gmail.com>
2020-05-08 15:03:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200508145956) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
