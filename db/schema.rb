dde28c7171fe89af6c9f7296f6acea97637b0ebb
Carla Holcomb <carla.programmer@gmail.com>
2020-05-08 15:01:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200508145956) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
