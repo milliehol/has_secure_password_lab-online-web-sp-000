ba7da647a76a8feecb0a6b446dcef8e78e179900
Carla Holcomb <carla.programmer@gmail.com>
2020-05-08 14:37:29 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  
+  def create
+        return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+                       
+        session[:name] = params[:name]
+        redirect_to '/'
+   end
 end
