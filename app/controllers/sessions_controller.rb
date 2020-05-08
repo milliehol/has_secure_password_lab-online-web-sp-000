e2f66a1197aa70cc3a611276f1e7449e99190ca5
Carla Holcomb <carla.programmer@gmail.com>
2020-05-08 14:44:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   redirect_to '/'
   end
   
-   def post
+   def create
         return redirect_to(controller: 'sessions',
                        action: 'new') if !params[:name] || params[:name].empty?
                        
