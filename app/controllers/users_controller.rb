9fec80d9385bcfbc5edbb7f5ee6a5b6466d1e2c7
Carla Holcomb <carla.programmer@gmail.com>
2020-05-08 14:48:44 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   
+  def new
+  end
+
   def create
-        return redirect_to(controller: 'sessions',
-                       action: 'new') if !params[:name] || params[:name].empty?
-                       
-        session[:name] = params[:name]
-        redirect_to '/'
-   end
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+  
 end
